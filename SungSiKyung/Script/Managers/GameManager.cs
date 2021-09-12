614132291418b48c46eaf57e73705a22ef5e9eed
dubious123 <dubious123@hanyang.ac.kr>
2021-09-12 10:15:10 +0900

    09.12.2021 #3

--- SungSiKyung/Script/Managers/GameManager.cs
+++ SungSiKyung/Script/Managers/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using SungSiKyung.Script.Utils;
+﻿using SungSiKyung.Script.Content;
+using SungSiKyung.Script.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,24 @@ namespace SungSiKyung.Script.Managers
 {
     public class GameManager
     {
+        Player _player;
+        public Player CurrentPlayer 
+        { get
+            {
+                if(_player == null) { CreatePlayer(); }
+                return _player;
+            } 
+        }
         public void Init()
         {
 
         }
+        void CreatePlayer()
+        {
+            _player = new Player();
+        }
         public void StartScene()
-        {      
+        {
             while (true)
             {
                 Managers.InputMgr.GetInput();
