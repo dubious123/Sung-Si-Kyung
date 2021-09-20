c76353cf4f216a77389beb72525af1b669d6bb2d
ohhoho8 <haeryung8@gmail.com>
2021-09-20 19:21:34 +0900

    cha

---
+++ SungSiKyung/Data/ImageLibrary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SungSiKyung.Data
+{
+    public class ImageLibrary :BaseLibrary
+    {
+
+        public static int[,] pixel1 = {{ 0, 1, 1, 1, 1 },{0,1,1,1,1},{0,1,1,1,1},{0,1,1,1,1},{0,0,1,0,0},{0,1,1,1,0},
+            {1,0,1,0,1},{1,0,1,0,1},{0,0,1,0,0},{0,1,0,1,0},{1,0,0,0,1},{1,0,0,0,1} };
+        public static int[,] pixel2 = {{0,0,0,0,0},{0,1,1,1,1},{ 0,1,1,1,1},{ 0,1,1,1,1},{ 0,1,1,1,1},{ 0,0,1,0,0},
+            { 0,1,1,1,0},{ 1,0,1,0,1},{ 1,0,1,0,1},{ 0,1,0,1,0},{ 1,0,0,0,1},{ 1,0,0,0,1}};
+        Dictionary<string, Image> _imageDict = new Dictionary<string, Image>();
+
+        public ImageLibrary()
+        {
+            _imageDict.Add("idle1",new Image(pixel1));
+            _imageDict.Add("idle2",new Image(pixel2));
+        }
+
+
+        public override Data FindBook(string id)
+        {
+            if (_imageDict.ContainsKey(id))
+            {
+                Image input = new Image(_imageDict[id].inputPixel);
+                return input;
+            }
+
+            else
+                return null;
+        }
+    }
+}
---
+++ SungSiKyung/Data/Librarys.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SungSiKyung.Data
+{
+    public static class Librarys
+    {
+        static HashSet<BaseLibrary> _librarySet;
+        static Librarys()
+        {
+            _librarySet = new HashSet<BaseLibrary>();
+            _librarySet.Add(new ImageLibrary());
+            
+        }
+        public static Dictionary<string, Data> dicTotal = new Dictionary<string, Data>()
+        {
+
+            {"idle1", ImageLibrary.GetImage("idle1") },
+            {"idle2", new Image(ImageLibrary.pixel2) }
+        };
+
+        public static T Load<T>(string id) where T : Data
+        {
+
+            if (dicTotal[id] is Image) { return ImageLibrary.GetImage(id).load() as T; }
+            else
+                return default;
+
+        }
+    }
+}
