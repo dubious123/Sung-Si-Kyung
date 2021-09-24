b56c5460fd4fa14a75335fe6aba5bc76c9493e58
윤재 노 <yjroh0928@naver.com>
2021-09-24 18:45:31 +0900

    .

--- SungSiKyung/Script/Managers/RenderingManager.cs
+++ SungSiKyung/Script/Managers/RenderingManager.cs
@@ -23,16 +23,32 @@ namespace SungSiKyung.Script.Managers
             //Test
             _sb = new StringBuilder();
             Console.CursorVisible = false;
-            Console.SetWindowSize(Console.LargestWindowWidth/2, (int)(Console.LargestWindowHeight * 0.8));
-            Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            SetWindowSize();
+            //Console.SetWindowSize(Console.LargestWindowWidth/2, (int)(Console.LargestWindowHeight * 0.8));
+            //Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
             _builder = new char[Console.WindowHeight][];
-            for (int i = 0 ; i< Console.WindowHeight; i++)
+            for (int i = 0; i < Console.WindowHeight; i++)
             {
-                _builder[i] = new char[Console.LargestWindowWidth / 2];
-                for (int j = 0;j<Console.WindowWidth;j++)
+                _builder[i] = new char[Console.WindowWidth];
+                for (int j = 0; j < Console.WindowWidth; j++)
                 {
                     _builder[i][j] = ' ';
+
                 }
+            }
+        }
+        void SetWindowSize()
+        {
+            try
+            {
+                Console.SetWindowSize(Define.ConsoleWidth, Define.ConsoleHeight);
+                Console.SetBufferSize(Define.ConsoleWidth * 2, Define.ConsoleHeight);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("콘솔 글꼴 크기를 줄여주세요! (권장 6)");
+                Console.SetCursorPosition(0, 0);
+                SetWindowSize();
             }
         }
         void ClearConsole()
@@ -41,7 +57,7 @@ namespace SungSiKyung.Script.Managers
             Console.SetCursorPosition(0, 0);
             for (int i = 0; i < Console.WindowHeight; i++)
             {
-                _builder[i] = new char[Console.LargestWindowWidth / 2];
+                _builder[i] = new char[Console.WindowWidth];
                 for (int j = 0; j < Console.WindowWidth; j++)
                 {
                     _builder[i][j] = ' ';
@@ -51,13 +67,14 @@ namespace SungSiKyung.Script.Managers
         public void RenderScene()
         {
             ClearConsole();
+            RenderBG();
             RenderDynamic();
             RenderStatic();
-            for (int y = 0; y<_builder.GetLength(0) -1;y++)
+            for (int y = 0; y < _builder.GetLength(0) - 1; y++)
             {
                 _sb.AppendLine(new string(_builder[y]));
             }
-            _sb.Append(new string(_builder[Console.WindowHeight-1]));
+            _sb.Append(new string(_builder[Console.WindowHeight - 1]));
             Console.Write(_sb);
         }
         void RenderDynamic()
@@ -73,6 +90,13 @@ namespace SungSiKyung.Script.Managers
             {
                 static_go.PrintStatic();
             }
+        }
+        void RenderBG() //배경
+        {
+            RenderData RenderingData = Librarys.Find<Image>("BG1");
+            Vector2Int pos = new Vector2Int(Define.ConsoleWidth / 2, Define.ConsoleHeight / 2);
+            (RenderingData as Image).PrintImage(pos);
+            // 아직 출력만
         }
     }
 }
