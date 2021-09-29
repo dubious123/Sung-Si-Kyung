220f189e56f5f406346507a8d2ccb3109afc8171
윤재 노 <yjroh0928@naver.com>
2021-09-29 14:00:47 +0900

    rendering

--- SungSiKyung/Script/Managers/RenderingManager.cs
+++ SungSiKyung/Script/Managers/RenderingManager.cs
@@ -18,47 +18,60 @@ namespace SungSiKyung.Script.Managers
         public char[][] ScreenBuilder { get { return _builder; } }
         char[][] _builder;
         StringBuilder _sb;
+        int _builderWidthLength;
+        int _builderHeightLength;
         public void Init()
         {
             //Test
             _sb = new StringBuilder();
             Console.CursorVisible = false;
+            Console.SetWindowSize(224, 63);
+            //Console.SetBufferSize(224, 63);
             SetWindowSize();
-            //Console.SetWindowSize(Console.LargestWindowWidth/2, (int)(Console.LargestWindowHeight * 0.8));
-            //Console.SetBufferSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+
+            _builderWidthLength = Console.WindowWidth;
+            _builderHeightLength = Console.WindowHeight;
+
             _builder = new char[Console.WindowHeight][];
             for (int i = 0; i < Console.WindowHeight; i++)
             {
                 _builder[i] = new char[Console.WindowWidth];
+                /*
                 for (int j = 0; j < Console.WindowWidth; j++)
                 {
                     _builder[i][j] = ' ';
 
                 }
+                */
             }
         }
         void SetWindowSize()
         {
-            try
+            Console.SetBufferSize(Define.ConsoleWidth, Define.ConsoleHeight);
+            while (true)
             {
-                Console.SetWindowSize(Define.ConsoleWidth, Define.ConsoleHeight);
-                Console.SetBufferSize(Define.ConsoleWidth * 2, Define.ConsoleHeight);
+                try
+                {
+                    Console.SetWindowSize(Define.ConsoleWidth, Define.ConsoleHeight);
+                    break;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("콘솔 글꼴 크기를 줄여주세요! (권장 6)");
+                    Console.SetCursorPosition(0, 0);
+                }
             }
-            catch (ArgumentOutOfRangeException)
-            {
-                Console.WriteLine("콘솔 글꼴 크기를 줄여주세요! (권장 6)");
-                Console.SetCursorPosition(0, 0);
-                SetWindowSize();
-            }
+           
+            
         }
         void ClearConsole()
         {
             _sb.Clear();
             Console.SetCursorPosition(0, 0);
-            for (int i = 0; i < Console.WindowHeight; i++)
+            for (int i = 0; i < _builderHeightLength; i++)
             {
-                _builder[i] = new char[Console.WindowWidth];
-                for (int j = 0; j < Console.WindowWidth; j++)
+                _builder[i] = new char[_builderWidthLength];
+                for (int j = 0; j < _builderWidthLength; j++)
                 {
                     _builder[i][j] = ' ';
                 }
@@ -97,6 +110,15 @@ namespace SungSiKyung.Script.Managers
             Vector2Int pos = new Vector2Int(Define.ConsoleWidth / 2, Define.ConsoleHeight / 2);
             (RenderingData as Image).PrintImage(pos);
             // 아직 출력만
+        }
+        void RenderBuilder()
+        {
+            for (int y = 0; y < _builder.GetLength(0) - 1; y++)
+            {
+                _sb.AppendLine(new string(_builder[y]));
+            }
+            _sb.Append(new string(_builder[Console.WindowHeight - 1]));
+            Console.Write(_sb);
         }
     }
 }
